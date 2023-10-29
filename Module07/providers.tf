ab340246962bf68d6d0ce6bbb3d597263eea761f
@henriwle <henriwle@stud.ntnu.no>
2023-10-29 20:35:10 +0100

    Module07 creation

---
+++ Module07/providers.tf
@@ -0,0 +1,23 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "3.73.0"
+    }
+  }
+  backend "azurerm" {
+    resource_group_name  = "rg-backend-tfstate-hwl"
+    storage_account_name = "sabetfsjph7asfaoh"
+    container_name       = "tfstate"
+    key                  = "web.terraform.tfstate"
+  }
+}
+
+provider "azurerm" {
+  features {
+    key_vault {
+      purge_soft_delete_on_destroy    = true
+      recover_soft_deleted_key_vaults = true
+    }
+  }
+}
