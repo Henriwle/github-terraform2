ae448d4f678b16777d1e3c1c956222941ded4bdc
@henriwle <henriwle@stud.ntnu.no>
2023-11-05 15:22:45 +0100

    critical-threat-fix

--- Module07/storageaccount.tf
+++ Module07/storageaccount.tf
@@ -6,6 +6,7 @@ resource "azurerm_storage_account" "sa" {
   location                 = azurerm_resource_group.rg-infra.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
+  min_tls_version          = "TLS1_2"
 }
 
 resource "azurerm_storage_container" "sc" {
